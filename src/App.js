06501b4be71ead87bbd395c58764aa6691ecb8a8
nhusn <139863367+nhusn@users.noreply.github.com>
2023-10-27 18:34:05 +0530

    Completed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [styles,setStyle] = useState(
+    {
+      width:"100%",
+      height:"100vh",
+      backgroundColor:"#18bc9c"
+    }
+  )
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={styles} className="d-flex justify-content-center align-items-center">
+      
+        <div style={{border:"solid black 1px"}} className='bg-light p-3 rounded w-50 d-flex justify-content-between align-items-center'>
+          <button onClick={()=>setStyle({...styles,backgroundColor:"#e74c3c"})} className='btn btn-danger'>Red</button>
+          <button onClick={()=>setStyle({...styles,backgroundColor:"#2c3e50"})} className='btn btn-primary'>Black</button>
+          <button onClick={()=>setStyle({...styles,backgroundColor:"#95a5a6"})} className='btn btn-secondary'>Hash</button>
+          <button onClick={()=>setStyle({...styles,backgroundColor:"#3498db"})} className='btn btn-info'>Blue</button>
+          <button onClick={()=>setStyle({...styles,backgroundColor:"#f39c12"})} className='btn btn-warning'>Yellow</button>
+          <button onClick={()=>setStyle({...styles,backgroundColor:"#ecf0f1"})} className='btn btn-light border'>white</button>
+          <button onClick={()=>setStyle({...styles,backgroundColor:"#18bc9c"})} className='btn btn-success '>Green</button>
+        </div>
+       
+      
     </div>
   );
 }
